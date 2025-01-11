51ced64f92b3cbd456a9b9a8a93726fd1d529c0c
AkotechIntelligence <akotechintelligence@gmail.com>
2025-01-11 19:41:17 +0000

    fixed database issues

--- src/controllers/HomePageController.js
+++ src/controllers/HomePageController.js
@@ -26,7 +26,7 @@ Controller.getHomePage = async (req, res) => {
 
 		// Get featured products with their images using a JOIN query
 		const featuredProducts = await db.sequelize.query(`
-			SELECT p.*, pi.imageUrl 
+			SELECT p.*, pi."imageUrl"
 			FROM "Product" p
 			LEFT JOIN "ProductImages" pi ON p.uuid = pi."productUuid"
 			ORDER BY p."createdAt" DESC
--- src/database/config/database.js
+++ src/database/config/database.js
@@ -8,7 +8,7 @@ module.exports = {
 		operatorsAliases: 0,
 		define: {
 			timestamp: true,
-			underscored: true,
+			underscored: false,
 		},
 		pool: {
 			max: 5,
